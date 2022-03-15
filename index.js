2518ee91665c71e2b7b4872f7c40d846cf50f0c5
SAOJulius <75691996+SAOJulius@users.noreply.github.com>
2022-03-15 13:37:57 +0100

    .

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
+const path = require("path")
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
